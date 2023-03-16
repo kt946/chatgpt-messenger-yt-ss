e0ccb61f02b98788c3144986979fa01e2648aa95
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-16 07:35:29 -0400

    added Sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,21 @@
-import '../styles/globals.css'
+import SideBar from '../components/SideBar';
+import '../styles/globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
       <body>
-        <div className='flex'>
-
-          {/* Sidebar */}
+        <div className="flex">
+          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+            <SideBar />
+          </div>
 
           {/* ClientProvider - Notification */}
 
-          <div className='bg-[#343541] flex-1'>
-            {children}
-          </div>
+          <div className="bg-[#343541] flex-1">{children}</div>
         </div>
-      
       </body>
     </html>
-  )
+  );
 }

f4b530ed4f2c7b3a48ba1b425bf3071eb0a881de
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-17 14:27:03 -0400

    added sign out funcionality

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,11 @@
-import NewChat from "./NewChat";
+'use client';
+
+import { signOut, useSession } from 'next-auth/react';
+import NewChat from './NewChat';
 
 function SideBar() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -8,13 +13,21 @@ function SideBar() {
           {/* New Chat */}
           <NewChat />
 
-          <div>
-            {/* Model Selection */}
-          </div>
+          <div>{/* Model Selection */}</div>
 
           {/* Map through Chat Rows */}
         </div>
       </div>
+
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="Profile pic"
+          referrerPolicy="no-referrer"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   );
 }

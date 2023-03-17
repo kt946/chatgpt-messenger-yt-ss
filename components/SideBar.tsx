e2b538536b0aea64061699cecdaf8559be720158
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-17 15:22:54 -0400

    added ChatRow component and delete funtionality

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { collection, orderBy, query } from 'firebase/firestore';
 import { signOut, useSession } from 'next-auth/react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { db } from '../firebase';
+import ChatRow from './ChatRow';
 import NewChat from './NewChat';
 
 function SideBar() {
   const { data: session } = useSession();
 
+  const [chats, loading, error] = useCollection(
+    session && query(collection(db, 'users', session.user?.email!, 'chats'), 
+    orderBy('createdAt', 'asc'))
+  );
+
+  console.log(chats);
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -15,6 +25,12 @@ function SideBar() {
           <div>{/* Model Selection */}</div>
 
           {/* Map through Chat Rows */}
+          {chats?.docs.map((chat) => (
+            <ChatRow
+              key={chat.id}
+              id={chat.id}
+            />
+          ))}
         </div>
       </div>
 

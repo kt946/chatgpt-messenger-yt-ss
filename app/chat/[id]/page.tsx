115c2f78ad174412f709c2ba59906d31eb1b1635
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-18 18:16:16 -0400

    added Chat and ChatInput components to homepage

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,7 +1,19 @@
-function ChatPage() {
+import Chat from '../../../components/Chat';
+import ChatInput from '../../../components/ChatInput';
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+function ChatPage({ params: { id } }: Props) {
   return (
-    <div>ChatPage</div>
-  )
+    <div className="flex flex-col h-screen overflow-hidden">
+      <Chat chatId={id} />
+      <ChatInput chatId={id} />
+    </div>
+  );
 }
 
-export default ChatPage
+export default ChatPage;
---
+++ components/Chat.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  chatId: String;
+};
+
+function Chat({ chatId }: Props) {
+  return <div className="flex-1">Chat</div>;
+}
+
+export default Chat;

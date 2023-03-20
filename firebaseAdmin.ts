82bfe560f322bf5426feb53cb3912f2f42928889
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-20 15:32:35 -0400

    added message component, updated chat component, and fixed import statements

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import { getApps } from 'firebase/app';
+import { getApps } from 'firebase-admin/app';
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 

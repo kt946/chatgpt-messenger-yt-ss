8176f0b06ca21d68281a1c85f188f7fa337b18a0
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-20 16:19:31 -0400

    added model selection component

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -6,6 +6,8 @@ import { useSession } from 'next-auth/react';
 import { FormEvent, useState } from 'react';
 import { db } from '../firebase';
 import toast from 'react-hot-toast';
+import ModelSelection from './ModelSelection';
+import useSWR from 'swr';
 
 type Props = {
   chatId: string;
@@ -15,8 +17,9 @@ function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState('');
   const { data: session } = useSession();
 
-  // useSWR to get model
-  const model = 'text-davinci-003';
+  const { data: model } = useSWR('model', {
+    fallbackData: 'text-davinci-003',
+  });
 
   const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -81,7 +84,9 @@ function ChatInput({ chatId }: Props) {
         </button>
       </form>
 
-      <div>{/* ModelSelection */}</div>
+      <div className="sm:hidden">
+        <ModelSelection />
+      </div>
     </div>
   );
 }
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -5,32 +5,41 @@ import { signOut, useSession } from 'next-auth/react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { db } from '../firebase';
 import ChatRow from './ChatRow';
+import ModelSelection from './ModelSelection';
 import NewChat from './NewChat';
 
 function SideBar() {
   const { data: session } = useSession();
 
   const [chats, loading, error] = useCollection(
-    session && query(collection(db, 'users', session.user?.email!, 'chats'), 
-    orderBy('createdAt', 'asc'))
+    session && query(collection(db, 'users', session.user?.email!, 'chats'), orderBy('createdAt', 'asc'))
   );
 
-  console.log(chats);
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
         <div>
           <NewChat />
 
-          <div>{/* Model Selection */}</div>
+          <div className="hidden sm:inline">
+            <ModelSelection />
+          </div>
 
-          {/* Map through Chat Rows */}
-          {chats?.docs.map((chat) => (
-            <ChatRow
-              key={chat.id}
-              id={chat.id}
-            />
-          ))}
+          <div className="flex flex-col space-y-2 my-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading Chats ...</p>
+              </div>
+            )}
+
+            {/* Map through Chat Rows */}
+            {chats?.docs.map((chat) => (
+              <ChatRow
+                key={chat.id}
+                id={chat.id}
+              />
+            ))}
+          </div>
         </div>
       </div>
 

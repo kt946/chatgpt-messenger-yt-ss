3aa25f370b4ddc886425b140f56ab7fac88794e0
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-20 14:37:28 -0400

    added firebase admin and updated askQuestion api

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import query from '../../lib/queryAPI';
+import admin from 'firebase-admin';
+import { adminDb } from '../../firebaseAdmin';
 
 type Data = {
   answer: string;
@@ -24,8 +26,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   const message: Message = {
     text: response || 'ChatGPT was unable to find an answer for that!',
-    
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: 'ChatGPT',
+      name: 'ChatGPT',
+      avatar: 'https://links.papareact.com/89k',
+    },
   };
 
-  res.status(200).json({ name: 'John Doe' });
+  await adminDb
+    .collection('users')
+    .doc(session?.user?.email)
+    .collection('chats')
+    .doc(chatId)
+    .collection('messages')
+    .add(message);
+
+  res.status(200).json({ answer: message.text });
 }
